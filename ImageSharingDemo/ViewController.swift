16c5b0f88998812cc558590de69e0fdf1495ccf3
Nissi Miranda <nissivm@yahoo.com.br>
2016-02-26 11:28:55 -0300

    Initial Commit

---
+++ ImageSharingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSharingDemo
+//
+//  Created by Nissi Vieira Miranda on 2/26/16.
+//  Copyright © 2016 Nissi Vieira Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
